3ac846bcd1f2b6e055342583188ace19a83a571e
ian15963 <ian.santos@sptech.school>
2025-04-17 15:55:32 -0300

    feat: Criando um converter customizado.

--- src/main/java/cadastro/empresas/aplicacao/controller/EmpresaBean.java
+++ src/main/java/cadastro/empresas/aplicacao/controller/EmpresaBean.java
@@ -4,13 +4,17 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
 
+import javax.faces.convert.Converter;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import cadastro.empresas.aplicacao.converter.RamoAtividadeConverter;
 import cadastro.empresas.aplicacao.model.Empresa;
+import cadastro.empresas.aplicacao.model.RamoAtividade;
 import cadastro.empresas.aplicacao.model.enums.TipoEmpresa;
 import cadastro.empresas.aplicacao.repository.EmpresaRepository;
+import cadastro.empresas.aplicacao.repository.RamoAtividadeRepository;
 import cadastro.empresas.aplicacao.util.CustomFacesMessage;
 
 @Named
@@ -20,16 +24,31 @@ public class EmpresaBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Inject
-	private EmpresaRepository repository;	
+	private EmpresaRepository repository;
+	@Inject
+	private RamoAtividadeRepository ramoAtividadeRepository;
 	private List<Empresa> empresas;
-	private Empresa empresa = new Empresa();
+	private Empresa empresa;
 	private String razaoSocial;
+	private Converter<RamoAtividade> converter;
 	
 	public void pesquisarEmpresa() {
 		empresas = repository.search(razaoSocial);
 		if(empresas.isEmpty()) {
 			CustomFacesMessage.info("Nenhuma empresa encontrada");
 		}
+	}
+	
+	public void prepararEmpresa() {
+		empresa = new Empresa();
+	}
+	
+	public List<RamoAtividade> completarRamoAtividade(String termo){
+		List<RamoAtividade> ramos = ramoAtividadeRepository.search(termo);
+		
+		converter = new RamoAtividadeConverter(ramos);
+		
+		return ramos;
 	}
 
 	public void listarEmpresas() {
@@ -68,5 +87,15 @@ public class EmpresaBean implements Serializable{
 	public List<Empresa> getEmpresas(){
 		return empresas;
 	}
+
+	public Converter getConverter() {
+		return converter;
+	}
+
+	public void setConverter(Converter converter) {
+		this.converter = converter;
+	}
+	
+	
 	
 }
---
+++ src/main/java/cadastro/empresas/aplicacao/converter/RamoAtividadeConverter.java
@@ -0,0 +1,45 @@
+package cadastro.empresas.aplicacao.converter;
+
+import java.util.List;
+import java.util.Objects;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+
+import cadastro.empresas.aplicacao.model.RamoAtividade;
+
+public class RamoAtividadeConverter implements Converter<RamoAtividade>{
+
+	private List<RamoAtividade> ramos;
+	
+	public RamoAtividadeConverter(List<RamoAtividade> ramos) {
+		super();
+		this.ramos = ramos;
+	}
+
+	@Override
+	public RamoAtividade getAsObject(FacesContext context, UIComponent component, String value) {
+		if(Objects.isNull(value)) {
+			return null;
+		}
+		
+		long ramoId = Long.valueOf(value);
+		
+		return ramos.stream()
+						.filter(ramo -> ramo.getId()
+						.equals(ramoId))
+						.findFirst()
+						.orElse(null);
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, RamoAtividade value) {
+		if(Objects.isNull(value)) {
+			return null;
+		}
+		
+		return value.getId().toString();
+	}
+
+}
