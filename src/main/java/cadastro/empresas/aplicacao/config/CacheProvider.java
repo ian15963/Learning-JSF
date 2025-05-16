ae1a9944482f3ce4d02624d8a2f08d4c7c0920d0
ian15963 <ian.santos@sptech.school>
2025-05-16 14:46:52 -0300

    feat: Adicionando configuração de TTL.

--- src/main/java/cadastro/empresas/aplicacao/config/CacheProvider.java
+++ src/main/java/cadastro/empresas/aplicacao/config/CacheProvider.java
@@ -1,17 +1,24 @@
 package cadastro.empresas.aplicacao.config;
 
+import java.time.Duration;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 
+import org.apache.commons.collections4.map.PassiveExpiringMap.ConstantTimeToLiveExpirationPolicy;
+import org.apache.commons.collections4.map.PassiveExpiringMap.ExpirationPolicy;
 import org.ehcache.Cache;
 import org.ehcache.CacheManager;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.expiry.Expirations;
+import org.ehcache.expiry.ExpiryPolicy;
 
 @ApplicationScoped
-public class CacheManagerConfig {
+public class CacheProvider {
 
 	private CacheManager cacheManager;
 	private Cache<String, Object> cache;
@@ -25,6 +32,7 @@ public class CacheManagerConfig {
 							Object.class,
 							ResourcePoolsBuilder.heap(100)
 					)
+					.withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofMinutes(15)))
 				)
 				.build();
 		
